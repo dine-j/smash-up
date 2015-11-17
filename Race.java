f563868dc8ba80cf5c4cef1a6a696771a638966d
dine-j <dine.j.neko@gmail.com>
2015-11-17 13:03:51 +0000

    Improved oriented-object

--- Race.java
+++ Race.java
@@ -1,30 +1,39 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Race
 {
-	private String[] race = {"Pirates", "Ninja", "Zombies", "Robots", "Dinosaures", "Sorciers", "Petit Peuple", "Aliens"};
-	private static String[] racesDispo = race;
+	private ArrayList<String> race = new ArrayList<String>();
 	
 	public Race()
 	{
+		String[] races = {"Pirates", "Ninja", "Zombies", "Robots", "Dinosaures", "Sorciers", "Petit Peuple", "Aliens"};
+		for(int i = 0; i < races.length; i++)
+		{
+			race.add(races[i]);
+		}
+	}
 	
+	public ArrayList<String> getRaces()
+	{
+		return race;
 	}
 	
 	public String[] getRandom2Races()
 	{
 		Random random = new Random();
-		int a = random.nextInt(race.length);
+		int a = random.nextInt(race.size());
 		int b;
 		do
 		{
-			b = random.nextInt(race.length);
+			b = random.nextInt(race.size());
 		} while(b == a);
 		
-		String[] choix = {racesDispo[a], racesDispo[b]};
+		String[] choix = {race.get(a), race.get(b)};
+		race.remove(a);
+		race.remove(b);
 		return choix;
-		
-		racesDispo = racesDispo.Where(w => w != racesDispo[a]).ToArray;
-		racesDispo = racesDispo.Where(w => w != racesDispo[b]).ToArray;
 	}
 }
--- SmashUp.java
+++ SmashUp.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SmashUp
-{
-	public static String[] race = {"Pirates", "Ninja", "Zombies", "Robots", "Dinosaures", "Sorciers", "Petit Peuple", "Aliens"};
-	
+{	
 	public static void main(String args[])
 	{
 		Scanner userInput = new Scanner(System.in);
-		ArrayList<String> raceDispo = new ArrayList<String>(Arrays.asList(race));
+		Race races = new Race();
 		
 		System.out.println("Combien de joueurs ?");
 		int nbJoueurs = userInput.nextInt();
@@ -20,29 +18,13 @@ public class SmashUp
 		
 		for(int i = 0; i < nbJoueurs; i++)
 		{
-			System.out.println("Joueur n°" + i + " ?");
-			joueur[i] = new Player(userInput.nextLine(), get2RandomRaces(raceDispo));
-			raceDispo.remove(joueur[i].getRaceJouee()[0]);
-			raceDispo.remove(joueur[i].getRaceJouee()[1]);
+			System.out.println("Joueur n°" + (i + 1) + " ?");
+			joueur[i] = new Player(userInput.nextLine(), races.getRandom2Races());
 		}
 		
 		for(int i = 0; i < nbJoueurs; i++)
 		{
 			System.out.println(joueur[i]);
 		}
-	}
-	
-	public static String[] get2RandomRaces(ArrayList<String> race)
-	{
-		Random random = new Random();
-		String[] choix = new String[2];
-		
-		for(int i = 0; i < 2; ++i)
-		{
-			choix[i] = race.get(random.nextInt(race.size()));
-			race.remove(choix[i]);
-		}
-		
-		return choix;
 	}
 }
